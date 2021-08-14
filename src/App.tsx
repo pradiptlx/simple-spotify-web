5318a104d24c03ed8ff65c1df9cfd62b9724857d
Dex <prdptlx@gmail.com>
2021-08-14 13:48:06 +0700

    feat: main routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,204 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react/jsx-props-no-spreading */
+import React from "react";
+import "./App.css";
+import {
+  useAppDispatch as useDispatch,
+  useAppSelector as useSelector,
+} from "redux/store";
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Redirect,
+  RouteComponentProps,
+} from "react-router-dom";
+import {
+  setAccessToken,
+  setExpiredTokenTime,
+  setLoginState,
+} from "./redux/actions/authorization";
+import { setCurrentUser } from "./redux/actions/user";
+import { fetchCurrentUserProfile } from "./api/fetch";
+import { parsingAccessToken } from "./utils/authorization";
+import Navbar from "./components/Navbar";
+import Home from "./pages";
+import CreatePlaylist from "./pages/CreatePlaylist";
+import Login from "./pages/Login";
 
-function App() {
+type AppRouteComponentProps = {
+  isLogin: boolean;
+  isPrivate: boolean;
+  component: (props: RouteComponentProps) => React.ReactElement;
+};
+
+type PrivateRouteProps = AppRouteComponentProps & {
+  exact: boolean;
+  path: string;
+};
+
+const PrivateRoute: React.FunctionComponent<PrivateRouteProps> = ({
+  component: Component,
+  ...props
+}) => (
+  <Route
+    {...props}
+    render={(routeProps) =>
+      !props.isLogin && props.isPrivate ? (
+        <Redirect
+          to={{
+            pathname: "/login",
+            state: { from: routeProps.location },
+          }}
+        />
+      ) : (
+        <Component {...routeProps} />
+      )
+    }
+  />
+);
+
+type localStorageAccessToken = {
+  accessToken: string;
+  expiredTokenTime: number;
+};
+
+function App(): React.ReactElement {
+  const dispatch = useDispatch();
+  const { isLogin, isTokenExpired, isAccessTokenExists, accessToken } =
+    useSelector((state) => state.authorization);
+  const currentUser = useSelector((state) => state.user);
+  const [isFirstMounted, setIsFirstMounted] = React.useState(true);
+
+  const routes = [
+    {
+      pathname: "/",
+      component: Home,
+      isPrivate: true,
+      state: null,
+    },
+    {
+      pathname: "/create-playlist",
+      component: CreatePlaylist,
+      isPrivate: true,
+      state: null,
+    },
+  ];
+
+  // When isLogin state is initialized, its state is false so handle with isFirstMounted to prevent redirecting to Login component each refresh
+  React.useEffect(() => {
+    if (isFirstMounted) {
+      setIsFirstMounted(false);
+    }
+  }, []);
+
+  // Parsing access token from local storage or from URL callback
+  React.useEffect(() => {
+    const {
+      accessToken: accessTokenLocal,
+      expiredTokenTime: expiredTimeLocal,
+    }: localStorageAccessToken = JSON.parse(
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      localStorage.getItem("spotify-access-token")!
+    ) || {
+      accessToken: "",
+      expiredTokenTime: new Date(),
+    };
+
+    const isExpired = new Date(expiredTimeLocal).getTime() - Date.now() <= 0;
+    const isTokenExist = Boolean(accessTokenLocal);
+    const url = window.location.href;
+
+    if (url.split("#").length > 1 && isExpired && !isLogin) {
+      parsingAccessToken(({ accessTokenURL, expiredTokenTimeURL }) => {
+        dispatch(
+          setAccessToken({
+            accessToken: accessTokenURL,
+            isAccessTokenExists: true,
+          })
+        );
+
+        dispatch(
+          setExpiredTokenTime({
+            isTokenExpired: false,
+            expiredTokenTime: expiredTokenTimeURL.getTime(),
+          })
+        );
+      });
+    } else if (!isLogin && isTokenExist && !isExpired) {
+      dispatch(setLoginState(true));
+    } else {
+      // Fallback conditional
+      dispatch(
+        setAccessToken({
+          accessToken: accessTokenLocal,
+          isAccessTokenExists: isTokenExist,
+        })
+      );
+
+      dispatch(
+        setExpiredTokenTime({
+          expiredTokenTime: expiredTimeLocal,
+          isTokenExpired: isExpired,
+        })
+      );
+    }
+  }, [isLogin]);
+
+  // Fetch user info
+  React.useEffect(() => {
+    if ((currentUser.id === "" || isTokenExpired) && isAccessTokenExists) {
+      fetchCurrentUserProfile(
+        { accessToken },
+        (userData) => {
+          dispatch(setCurrentUser(userData));
+        },
+        ({ statusCode }) => {
+          if (statusCode === 400 || statusCode === 401) {
+            dispatch(
+              setExpiredTokenTime({
+                expiredTokenTime: new Date().getTime(),
+                isTokenExpired: true,
+              })
+            );
+          }
+        }
+      );
+    }
+  }, [accessToken, isTokenExpired, isAccessTokenExists, currentUser.id]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          {/* Auth Handler */}
+          <Route path="/login" component={Login} />
+
+          {routes.map((route) => {
+            if (route.isPrivate) {
+              return (
+                <PrivateRoute
+                  key={`private_${route.pathname}`}
+                  exact // TODO: Change exact behaviour
+                  path={route.pathname}
+                  isPrivate
+                  isLogin={isLogin || isFirstMounted}
+                  component={route.component}
+                />
+              );
+            }
+            return (
+              <Route
+                key={`public_${route.pathname}`}
+                exact
+                path={route.pathname}
+                component={route.component}
+              />
+            );
+          })}
+        </Switch>
+      </BrowserRouter>
+    </>
   );
 }
 
