b666776f668b83d573c752c90e95bea9031e4a50
Akwila F <prdptlx@gmail.com>
2021-08-16 16:59:13 +0700

    fix(Tracks): concat key with idx
    Some playlists has duplicate tracks

--- src/components/Tracks/index.tsx
+++ src/components/Tracks/index.tsx
@@ -100,14 +100,15 @@ const Tracks: React.FC<tracksProps> = (props) => {
 
   return trackData.length && !isLoading ? (
     <>
-      {trackData.map((data) => (
+      {trackData.map((data, idx) => (
         <div
           className={`tracks flex-shrink-0 rounded-lg shadow-lg text-center mt-10 dark:bg-gray-700 ${
             !selectedTrack[data.id]?.active
               ? "transition duration-500 ease-in-out hover:bg-gray-200 dark:hover:bg-gray-900 transform hover:-translate-y-2 hover:scale-105"
               : ""
           }`}
-          key={data.uri}
+          // eslint-disable-next-line react/no-array-index-key
+          key={`${data.uri}_${idx}`}
           onClick={() => {
             trackOnClickHandler(data.id, data.uri);
           }}
