1a4cb1be933dbaa2e94e70efbbce05bbdc80c5e1
Akwila F <prdptlx@gmail.com>
2021-08-17 20:10:02 +0700

    refactor(PlaylistPage): use CardList component

--- src/pages/Playlist/index.tsx
+++ src/pages/Playlist/index.tsx
@@ -11,6 +11,60 @@ import { PlaylistObject, TrackObject } from "api/interfaces";
 import { errorArgFn, getCurrentUserPlaylists, getPlaylist } from "api/fetch";
 import { setExpiredTokenTime } from "redux/actions/authorization";
 import { setPageData } from "redux/actions/app";
+import Box from "@material-ui/core/Box";
+import Skeleton from "@material-ui/lab/Skeleton";
+import CardList from "components/CardList";
+
+const emptyDataComponent = () => (
+  <div className="flex flex-wrap justify-center items-stretch space-x-4">
+    {new Array(10).fill(0).map((_, idx) => (
+      <Box
+        // eslint-disable-next-line react/no-array-index-key
+        key={`box_${idx}`}
+        mt={10}
+        style={{
+          maxWidth: "24rem",
+          overflow: "hidden",
+        }}
+        data-testid="emptyTrackComponent"
+      >
+        <Skeleton
+          variant="rect"
+          animation="wave"
+          width={600}
+          height="300px"
+          style={{
+            borderRadius: "0.25rem",
+          }}
+        />
+        <Box
+          py={2}
+          px={4}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: ".25rem",
+          }}
+        >
+          <Skeleton
+            variant="text"
+            style={{
+              width: "300px",
+            }}
+          />
+          <Skeleton
+            variant="text"
+            style={{
+              width: "300px",
+            }}
+          />
+        </Box>
+      </Box>
+    ))}
+  </div>
+);
 
 const PlaylistPage = (): React.ReactElement => {
   const { playlistId } = useParams<{ playlistId: string }>();
@@ -101,9 +155,11 @@ const PlaylistPage = (): React.ReactElement => {
           <h1 className="text-3xl dark:text-white my-5">
             {currentPlalist?.name}
           </h1>
-          <div className="flex flex-wrap justify-center items-stretch space-x-4">
-            <Tracks trackData={tracks} />
-          </div>
+          <CardList
+            type="tracks"
+            cardListItems={tracks}
+            emptyDataComponentFn={emptyDataComponent}
+          />
         </div>
       </div>
     </div>
